6626ee0bf40ecd17e304a60f43ff7b1a5467730a
Andres Romero <af.romerob1@uniandes.edu.co>
2025-03-17 18:06:32 -0500

    Add animations to testimonials section, Add why us modals and Add mobile menu component

---
+++ src/data/whyUsModal.ts
@@ -0,0 +1,38 @@
+export const whyUsModal = [
+    {
+        title: "Atención Personalizada",
+        content: "Cada caso es único, y el tuyo no es la excepción. Te escuchamos, te guiamos y te acompañamos en cada paso del proceso, asegurándonos de que recibas el apoyo y la asesoría legal que necesitas."
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    },
+    {
+        title: "Why Us",
+        content: "We are the best because we are the best"
+    }
+]
